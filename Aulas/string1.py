a8330d95af169067cfaacfc2974a557396a347ca
rabmorim <ramorim13@yahoo.com.br>
2023-08-23 18:13:18 -0300

    Enviando Arquivo de Aulas

---
+++ Aulas/string1.py
@@ -0,0 +1,33 @@
+nome = "RoDrigo ABreu amorim"
+
+print(nome.upper())
+
+print(nome.lower())
+
+print(nome.title())
+
+    #print(nome.strip())
+
+print(nome.center(30, "#"))
+
+print("." .join(nome.title()),)
+
+
+print(nome[14:21:2])
+
+print(nome[::-1]) #espelhamento da string
+
+
+print("""
+        ############# MENU ############# 
+      
+      1- Sacar
+
+      2- Depositar
+
+      0- Sair
+
+        ################################ 
+      """
+
+) #String multipla
