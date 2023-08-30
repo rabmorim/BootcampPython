631e915510245c67ce405099b197d581b57e3716
rabmorim <ramorim13@yahoo.com.br>
2023-08-30 15:58:03 -0300

    Update_class

---
+++ Aulas/listas.py
@@ -0,0 +1,35 @@
+frutas = ['Maçã', 'Banana', 'Pera', 'Morango']
+
+# print(frutas)
+
+print(frutas[0])
+
+print(frutas[-1])
+
+print(frutas[-2])
+
+letras = list('Python') # Imprime para nós todas as letras dessa string como uma lista
+
+print(letras)
+
+numeros = list(range(0,10,2))
+
+print(numeros)
+
+matriz = [
+    [1,'a',3],
+    [2,'b',4],
+    [3,'c',5]
+]
+
+print(matriz[0])
+print(matriz[0][2])
+
+carros = ['Gol','Celta','Ferrari']
+
+for carro in carros:
+    print(carro)
+
+
+
+import pandas as pd
